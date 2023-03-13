1cd4370d45b688a23f943027a3d66ee09763cfc9
Andrew Bruner <andrewbruner@gmail.com>
2023-03-13 23:09:53 +0000

    Update view

--- scripts/view.js
+++ scripts/view.js
@@ -7,18 +7,31 @@ import controller from "./controller.js";
  */
 
 /**
- * Creates a new element.
- * @param {string} tagName - Type of element
- * @param {object} [options] - The options object, if any
- * @param {string[]} [options.classList] - Class names to add to element, if any
- * @param {{ type: string, callback: Callback, options?: { [string]: * } }[]} [options.listeners] - Event listeners to attach to element, if any
- * @param {{ [string]: * }} [options.attributes] - Element attributes, if any
- * @returns {Element} The newly created HTML element
+ * @param {HTMLElement} element
+ * @param	{*} attributes
  */
-function create(tagName, options) {
-	const element = document.createElement(tagName);
-	if (options.classList) {
-		element.classList.add(...options.classList);
+function setAttributes(element, attributes) {
+	for (const attribute in attributes) {
+		if (typeof attributes[attribute] === 'object') {
+			setAttributes(element[attribute], attributes[attribute]);
+		} else {
+			element[attribute] = attributes[attribute];
+		}
+	}
+}
+
+/**
+ * @param {string} tag
+ * @param {object} [options]
+ * @param {string[]} [options.classes]
+ * @param {{ type: string, callback: Callback, options?: { [string]: * } }[]} [options.listeners]
+ * @param {{ [string]: * }} [options.attributes]
+ * @returns {HTMLElement}
+ */
+function create(tag, options) {
+	const element = document.createElement(tag);
+	if (options.classes) {
+		element.classList.add(...options.classes);
 	}
 	if (options.listeners) {
 		options.listeners.forEach(listener => {
@@ -26,42 +39,38 @@ function create(tagName, options) {
 		});
 	}
 	if (options.attributes) {
-		for (const attribute in options.attributes) {
-			element[attribute] = options.attributes[attribute];
-		}
+		setAttributes(element, options.attributes);
 	}
 	return element;
 }
 
 const view = {
 	/**
-	 * INITIAL STAGE DONE
 	 * @param {Data} data
-	 * @param {string} key
-	 * @param {string} selector
 	 */
 	updateView(data) {
+		const stage = data.state.stage;
 		const app = document.querySelector('#app');
 		if (data.state.stage === 'initial') {
 			const header = create('header', { classList: ['alert', 'alert-primary', 'sticky-top', 'text-center'] });
-			const heading = create('h1', { attributes: { textContent: 'Select Distributor' } });
+				const heading = create('h1', { attributes: { textContent: 'Select Distributor' } });
 			header.append(heading);
 			app.append(header);
 			const main = create('main', { classList: ['container'] });
-			const form = create('form', {
-				classList: ['_distributor', 'd-flex', 'flex-column'],
-				listeners: [{ type: 'submit', callback: controller.handleSubmit }]
-			});
-			const cdcButton = create('button', {
-				classList: ['btn', 'btn-lg', 'btn-primary', 'mb-3'],
-				attributes: { textContent: 'CDC', type: 'submit', value: 'cdc' }
-			});
-			form.append(cdcButton);
-			const rdcButton = create('button', {
-				classList: ['btn', 'btn-lg', 'btn-primary'],
-				attributes: { textContent: 'RDC', type: 'submit', value: 'rdc' }
-			});
-			form.append(rdcButton);
+				const form = create('form', {
+					classList: ['_distributor', 'd-flex', 'flex-column'],
+					listeners: [{ type: 'submit', callback: controller.handleSubmit }]
+				});
+					const cdcButton = create('button', {
+						classList: ['btn', 'btn-lg', 'btn-primary', 'mb-3'],
+						attributes: { textContent: 'CDC', type: 'submit', value: 'cdc' }
+					});
+				form.append(cdcButton);
+					const rdcButton = create('button', {
+						classList: ['btn', 'btn-lg', 'btn-primary'],
+						attributes: { textContent: 'RDC', type: 'submit', value: 'rdc' }
+					});
+				form.append(rdcButton);
 			main.append(form);
 			app.append(main);
 		} else {
@@ -89,88 +98,87 @@ const view = {
 				},
 			};
 			const header = create('header', {
-				classList: ['alert', components.header.class[data.state.stage], 'sticky-top', 'text-center']
+				classList: ['alert', components.header.class[stage], 'sticky-top', 'text-center']
 			});
-			const heading = create('h1', { attributes: { textContent: components.heading.textContent[data.state.stage] } });
+				const heading = create('h1', { attributes: { textContent: components.heading.textContent[stage] } });
 			header.append(heading);
 			app.append(header);
 			const main = create('main', { classList: ['container'] });
-			const color = components.header.class[data.state.stage];
-			const form = create('form', {
-				listeners: [
-					{ type: 'focusin', callback: controller.handleFocusin, options: { color }},
-					{ type: 'focusout', callback: controller.handleFocusout, options: { color }},
-					{ type: 'submit', callback: controller.handleSubmit, }
-				]
-			});
-			data.items.forEach((item, index) => {
-				const division = create('div', {
-					classList: ['input-group', 'input-group-lg', 'row'],
-					listeners: [{ type: 'click', callback: controller.handleClick }],
-					attributes: { style: { cursor: 'pointer' } } // dataset: { index } ???
+				const color = components.header.class[stage];
+				const form = create('form', {
+					listeners: [
+						{ type: 'focusin', callback: controller.handleFocusin, color },
+						{ type: 'focusout', callback: controller.handleFocusout, color },
+						{ type: 'submit', callback: controller.handleSubmit }
+					]
 				});
-				const span = create('span', {
-					classList: ['align-items-start', 'col-7', 'd-flex', 'flex-column', 'input-group-text']
-				});
-				const description = create('span', { attributes: { textContent: item.description } });
-				span.append(description);
-				const details = create('span', {
-					classList: ['d-flex', 'fs-6', 'fw-light', 'justify-content-between', 'w-100']
-				});
-				const uom = create('span', { attributes: { textContent: `${item.uom}/cs` } });
-				details.append(uom);
-				const par = create('span', {
-					attributes: {
-						textContent: `${item.boh >= 0 ? item.boh + 'ea / ' : ''}${item.enRoute >= 0 ? item.enRoute + 'cs / ' : ''}${item.par}par`
-					}
-				});
-				details.append(par); 
-				span.append(details);
-				division.append(span);
-				const itemValue = item[data.state.stage];
-				const input = create('input', {
-					classList: ['col', 'form-control'],
-					attributes: {
-						type: 'tel',
-						value: itemValue !== null ? itemValue : '',
-						disabled: data.state.stage === 'order',
-						readOnly: data.state.stage === 'order'
-					}
-				});
-				division.append(input);
-				const units = create('span', {
-					classList: ['col-2', 'fs-6', 'input-group-text'],
-					attributes: { textContent: data.state.stage === 'boh' ? 'ea' : 'cs' }
-				});
-				division.append(units);
-				form.append(division);
-			});
-			const button = create('button', {
-				classList: ['btn', 'btn-lg', 'btn-primary', 'mb-5', 'mt-3'],
-				attributes: {
-					textContent: components.button.textContent[data.state.stage],
-					type: 'submit'
-				}
-			});
-			form.append(button);
+					data.items.forEach((item, index) => {
+						const division = create('div', {
+							classList: ['input-group', 'input-group-lg', 'row'],
+							listeners: [{ type: 'click', callback: controller.handleClick }],
+							attributes: { dataset: { index }, style: { cursor: 'pointer' } }
+						});
+							const span = create('span', {
+								classList: ['align-items-start', 'col-7', 'd-flex', 'flex-column', 'input-group-text']
+							});
+								const description = create('span', { attributes: { textContent: item.description } });
+							span.append(description);
+								const details = create('span', {
+									classList: ['d-flex', 'fs-6', 'fw-light', 'justify-content-between', 'w-100']
+								});
+									const uom = create('span', { attributes: { textContent: `${item.uom}/cs` } });
+								details.append(uom);
+									const par = create('span', {
+										attributes: {
+											textContent: `${item.boh >= 0 ? item.boh + 'ea / ' : ''}${item.enRoute >= 0 ? item.enRoute + 'cs / ' : ''}${item.par}par`
+										}
+									});
+								details.append(par); 
+							span.append(details);
+						division.append(span);
+							const value = item[stage];
+							const input = create('input', {
+								classList: ['col', 'form-control'],
+								attributes: {
+									type: 'tel',
+									value: value !== null ? value : '',
+									disabled: stage === 'order',
+									readOnly: stage === 'order'
+								}
+							});
+						division.append(input);
+							const units = create('span', {
+								classList: ['col-2', 'fs-6', 'input-group-text'],
+								attributes: { textContent: stage === 'boh' ? 'ea' : 'cs' }
+							});
+						division.append(units);
+						form.append(division);
+					});
+					const button = create('button', {
+						classList: ['btn', 'btn-lg', 'btn-primary', 'mb-5', 'mt-3'],
+						attributes: {
+							textContent: components.button.textContent[stage],
+							type: 'submit'
+						}
+					});
+				form.append(button);
 			main.append(form);
 			app.append(main);
 		}
 	},
 
 	/**
-	 * Updates single item to reflect current Data
 	 * @param {number} index 
 	 * @param {Data} data 
 	 */
 	updateItem(index, data) {
 		const item = data.items[index];
 		const form = document.querySelector('form');
-		const divisions = form.children;
-		const details = divisions[index].firstChild.lastChild;
-		const par = details.lastChild
-		par.textContent = `${item.boh >= 0 ? item.boh + 'ea / ' : ''}${item.enRoute >= 0 ? item.enRoute + 'cs / ' : ''}${item.par}par`;
-		details.append(par);
+			const divisions = form.children;
+				const details = divisions[index].firstChild.lastChild;
+					const par = details.lastChild
+					par.textContent = `${item.boh >= 0 ? item.boh + 'ea / ' : ''}${item.enRoute >= 0 ? item.enRoute + 'cs / ' : ''}${item.par}par`;
+				details.append(par);
 	},
 };
 
